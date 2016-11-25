09e2d880542cab168cd0f39f4240b0a5ef875b78
robin <none@none>
2016-11-25 12:43:35 +0000

    split data (forget road), more token changes and patterns; add AthlonsAgeGrader class

--- athlib/wma/agegrader.py
+++ athlib/wma/agegrader.py
@@ -4,32 +4,34 @@ __all__ =   (
 import json, re, os
 from collections import namedtuple
 _ = r"\d\.?\d*K"
-_throw_codes = re.compile(r"^(?:WT\d?%s|JT[45678]00|DT%s|HT%s|SP%s)$" % (_,_,_,_), re.IGNORECASE)
+_throw_codes = re.compile(r"^(?:WT(P<wtnum>\d?%s|)|JT(P<jtnum>[45678]00|)|DT(?P<dtnum>%s|)|HT(?P<htnum>%s|))|SP(?P<spnum>%s|)$" % (_,_,_,_), re.IGNORECASE)
 _jump_codes = re.compile(r"^(?:LJ|PV|TJ|HJ)$", re.IGNORECASE)
-_track_codes = re.compile(r"^\d+(?:H(?:3[36])?|SC)?$", re.IGNORECASE)
-_road_codes = re.compile(r"^(?:MILE|MAR|HM|\d+[MK]?)$", re.IGNORECASE)
+_track_codes = re.compile(r"^(?:(?P<meters>\d+)(?:[LS]?H(?:3[36])?|SC|W)?)|SC|[2345]MT|LH|SH$", re.IGNORECASE)
+_road_codes = re.compile(r"^(?:MILE|MAR|HM|\d+[MK]?)W?$", re.IGNORECASE)
 del _
 
 road_info = namedtuple('road_info','code distance standard factors')
 class AgeGrader(object):
     min_age = 35
     max_age = 100
-    _jump_codes = re.compile("^(?:LJ|PV|TJ|HJ)$", re.IGNORECASE)
-    _throw_codes = re.compile("^(WT|JT|DT|HT|SP)", re.IGNORECASE)
-    def __init__(self):
-        self._get_data()
+    data_year = "2015"
+    data_file_name = 'wma-data.json'
+    text_columns = 0,
+    event_column = 0
 
-    def _get_data(self):
+    def __init__(self,):
+        self._get_data(self.data_file_name)
+
+    def _get_data(self,data_file_name):
         from athlib import wma
-        with open(os.path.join(wma.__path__[0],'wma-data.json'),'rb') as f:
+        self.data_path = os.path.join(wma.__path__[0],data_file_name)
+        with open(self.data_path,'rb') as f:
             self.data = json.load(f)
 
     @property
     def _all_event_codes(self):
         data = self.data
-        return list(set([t[0] for T in (data['road']['m'],data['road']['f']) for t in T]+
-            [t[2] for T in (data['tf']['m'],data['tf']['f']) for t in T]))
-
+        return list(set([t[self.event_column] for T in (data[self.data_year]['m'],data[self.data_year]['f']) for t in T]))
 
     @staticmethod
     def _check_table_column(table,x,func):
@@ -50,15 +52,10 @@ class AgeGrader(object):
         def ccase(x):
             if x.upper()!=x:
                 nuc.append(x)
-        data = self.data
-        road = data['road']
-        tf = data['tf']
-        self._check_table_column(road['m'],0,ccase)
-        self._check_table_column(road['f'],0,ccase)
-        self._check_table_column(tf['m'],0,ccase)
-        self._check_table_column(tf['f'],0,ccase)
-        self._check_table_column(tf['m'],2,ccase)
-        self._check_table_column(tf['f'],2,ccase)
+        data = self.data[self.data_year]
+        for j in self.text_columns:
+            self._check_table_column(data['m'],j,ccase)
+            self._check_table_column(data['f'],j,ccase)
         if nuc:
             print 'these strings need uppercasing in the json %s' % ' '.join(repr(t) for t in list(set(nuc)))
 
@@ -93,28 +90,51 @@ class AgeGrader(object):
         0.9926
         >>> ag.calculate_factor('F',35.5,'200K')
         0.99095
-        >>> ag.calculate_factor('M',65,'100h')
-        0.8637
-        >>> ag.calculate_factor('M',69,'100H')
-        0.8637
         >>> ag.calculate_factor('M',65,'10000')
-        0.7858
+        0.7691
         >>> ag.calculate_factor('M',69,'10000')
-        0.7858
-        >>> ag.calculate_factor('f',35,'100H')
-        0.9852
-        >>> ag.calculate_factor('F',39,'100H')
-        0.9852
+        0.7402
         >>> ag.calculate_factor('F',35,'1500')
-        0.9872
+        0.9822
         >>> ag.calculate_factor('f',39,'1500')
-        0.9872
+        0.9547
+        >>> ag.calculate_factor('f',35,'SH')
+        0.9791
+        >>> ag.calculate_factor('f',39,'SH')
+        0.9576
+        >>> ag.calculate_factor('m',35,'LH')
+        0.9647
+        >>> ag.calculate_factor('m',39,'LH')
+        0.9254
         '''
         kind = self.event_code_to_kind(event)
-        if kind=='road':
-            return self.calculate_road_factor(gender,age,event)
+        event = event.upper()
+        gender = self.normalize_gender(gender)
+        #which table we're using
+        data = self.data[self.data_year]
+        table = data[gender]
+        ages = data['ages']
+        nt = len(table)
+        if distance is None:
+            #we must match an event exactly
+            self.find_row_by_event(event,table,x=self.event_column,label='wma.%s.%s'%(self.data_year,gender))
         else:
-            return self.calculate_tf_factor(gender,age,event)
+            self.find_row_by_distance(distance,table,x=1,label='wma.%s.%s'%(self.data_year,gender))
+        self.find_age(age,ages)
+        fx = self._fx
+        fx1 = self._fx1
+        pfac = self._pfac
+        ax = self._ax
+        ax1 = self._ax1
+        page = self._page
+        FX = table[fx][3:]
+        FX1 = table[fx1][3:]
+        fac =  FX[ax]
+        faca = FX[ax1]
+        fac1 = FX1[ax]
+        fac1a = FX1[ax1]
+        fac = (1-pfac)*(page*faca+(1-page)*fac)+pfac*(page*fac1a+(1-page)*fac1)
+        return fac
 
     @staticmethod
     def str2num(s):
@@ -202,84 +222,6 @@ class AgeGrader(object):
         self._ax1 = ax1
         self._page = page
 
-    def calculate_road_factor(self,gender,age,event,distance=None):
-        '''
-        >>> from athlib.wma.agegrader import AgeGrader
-        >>> ag=AgeGrader()
-        >>> ag.calculate_road_factor('M',68,'5K')
-        0.7592
-        >>> ag.calculate_road_factor('M',68,'200K')
-        0.7561
-        >>> ag.calculate_road_factor('M',68.5,'200K')
-        0.7522
-        >>> ag.calculate_road_factor('f',35,'5K')
-        0.9935
-        >>> ag.calculate_road_factor('f',35,'200K')
-        0.9926
-        >>> ag.calculate_road_factor('F',35.5,'200K')
-        0.99095
-        '''
-        event = event.upper()
-        gender = self.normalize_gender(gender)
-        #which table we're using
-        data = self.data['road']
-        table = data[gender]
-        ages = data['ages']
-        nt = len(table)
-        if distance is None:
-            #we must match an event exactly
-            self.find_row_by_event(event,table,x=0,label='road.%s'%gender)
-        else:
-            self.find_row_by_distance(distance,table,x=1,label='road.%s'%gender)
-        self.find_age(age,ages)
-        fx = self._fx
-        fx1 = self._fx1
-        pfac = self._pfac
-        ax = self._ax
-        ax1 = self._ax1
-        page = self._page
-        FX = table[fx][3:]
-        FX1 = table[fx1][3:]
-        fac =  FX[ax]
-        faca = FX[ax1]
-        fac1 = FX1[ax]
-        fac1a = FX1[ax1]
-        fac = (1-pfac)*(page*faca+(1-page)*fac)+pfac*(page*fac1a+(1-page)*fac1)
-        return fac
-
-    def calculate_tf_factor(self,gender,age,event):
-        '''
-        >>> from athlib.wma.agegrader import AgeGrader
-        >>> ag=AgeGrader()
-        >>> ag.calculate_tf_factor('M',65,'100H')
-        0.8637
-        >>> ag.calculate_tf_factor('M',69,'100H')
-        0.8637
-        >>> ag.calculate_tf_factor('M',65,'10000')
-        0.7858
-        >>> ag.calculate_tf_factor('M',69,'10000')
-        0.7858
-        >>> ag.calculate_tf_factor('f',35,'100H')
-        0.9852
-        >>> ag.calculate_tf_factor('F',39,'100H')
-        0.9852
-        >>> ag.calculate_tf_factor('F',35,'1500')
-        0.9872
-        >>> ag.calculate_tf_factor('f',39,'1500')
-        0.9872
-        '''
-        event = event.upper()
-        gender = self.normalize_gender(gender)
-        #which table we're using
-        data = self.data['tf']
-        table = data[gender]
-        ages = data['ages']
-        #we must match an event exactly
-        self.find_row_by_event(event,table,x=2,label='tf.%s'%gender)
-        self.find_age(int(age/5)*5,ages,interpolate=False)
-        fac =  table[self._fx][3:][self._ax1]
-        return fac
-
 
     def world_best(self, gender, event, performance):
         "The world best on the data stats were compiled for this event"
@@ -299,10 +241,15 @@ class AgeGrader(object):
         >>> from athlib.wma.agegrader import AgeGrader
         >>> ag=AgeGrader()
         >>> ag.calculate_age_grade('m',50,'5K', '16:23')
-        >>> 0.9004
+        performance = 983.00
+        world best for m 5K = 779.00
+        age group best would be 885.126690149
+        0.9004340693274128
         >>> ag.calculate_age_grade('f',50,'5K', '18:00')
-        >>> 0.9179
-
+        performance = 1080.00
+        world best for f 5K = 886.00
+        age group best would be 991.384133378
+        0.9179482716463806
         """
 
         #This works for jumps/throws too, as they are floats
@@ -310,14 +257,11 @@ class AgeGrader(object):
         print "performance = %0.2f" % float_performance
 
         kind = self.event_code_to_kind(event)
-        if kind != 'road':
-            raise NotImplementedError
-        data = self.data['road']
+        data = self.data[self.data_year]
         table = data[gender]
         row = self.find_row_by_event(event, table, x=0)
         world_best = table[row][2]
         
-
         print "world best for %s %s = %0.2f" % (gender, event, world_best)
 
         age_factor = self.calculate_factor(gender, age, event)
@@ -334,7 +278,43 @@ class AgeGrader(object):
 
         return age_grade
 
+class AthlonsAgeGrader(AgeGrader):
+    data_file_name="wma-athlons-data.json"
+    text_columns = 0,2
+    event_column = 2
 
+    def calculate_factor(self,gender,age,event):
+        '''
+        >>> from athlib.wma.agegrader import AthlonsAgeGrader
+        >>> ag=AthlonsAgeGrader()
+        >>> ag.calculate_factor('M',65,'100H')
+        0.8637
+        >>> ag.calculate_factor('M',69,'100H')
+        0.8637
+        >>> ag.calculate_factor('M',65,'10000')
+        0.7858
+        >>> ag.calculate_factor('M',69,'10000')
+        0.7858
+        >>> ag.calculate_factor('f',35,'100H')
+        0.9852
+        >>> ag.calculate_factor('F',39,'100H')
+        0.9852
+        >>> ag.calculate_factor('F',35,'1500')
+        0.9872
+        >>> ag.calculate_factor('f',39,'1500')
+        0.9872
+        '''
+        event = event.upper()
+        gender = self.normalize_gender(gender)
+        #which table we're using
+        data = self.data[self.data_year]
+        table = data[gender]
+        ages = data['ages']
+        #we must match an event exactly
+        self.find_row_by_event(event,table,x=2,label='wma-athlons.%s.%s'%(self.data_year,gender))
+        self.find_age(int(age/5)*5,ages,interpolate=False)
+        fac =  table[self._fx][3:][self._ax1]
+        return fac
 
 if __name__=='__main__':
     import doctest
