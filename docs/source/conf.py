c920e6c1d7ea4b725348e6b200dda48f05c941ff
Andy Robinson <captainrobbo@gmail.com>
2017-07-14 23:37:51 +0100

    tagged v 0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ author = u'Andy Robinson and others'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.0.9'
+version = u'0.1.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
