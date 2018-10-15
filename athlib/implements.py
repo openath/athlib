b18216f85f3a67523d7367baa97d77705834e7dd
Andy Robinson <captainrobbo@gmail.com>
2018-10-15 21:59:09 +0100

    extended get_implement_weights with Malta IAAF standards

--- athlib/implements.py
+++ athlib/implements.py
@@ -5,7 +5,9 @@ def get_implement_weight(event_code, gender, age_group):
     """Return standard weights in KG, as a formatted string
 
     Because UKA and IAAF have odd and even codes, hopefully
-    one function can do both.  Still looking for IAAF junior weights.
+    one function can do both.  
+
+    IAAF weights provided by Edwin Attard of Malta in Aug 2018
 
     See IAAF rules page 155
     """
@@ -14,9 +16,11 @@ def get_implement_weight(event_code, gender, age_group):
         if gender == "M":
             if age_group == "U13":
                 return "400"
-            elif age_group == "U15":
+            elif age_group == "U14":
+                return "500"
+            elif age_group in ["U15", "U16"]:
                 return "600"
-            elif age_group == "U17":
+            elif age_group in ["U17", "U18"]:
                 return "700"
             elif age_group in ("U20", "U23", "SEN", "V35", "V40", "V45"):
                 return "800"
@@ -29,11 +33,11 @@ def get_implement_weight(event_code, gender, age_group):
             elif age_group >= "V80":
                 return "700"
         if gender == "F":
-            if age_group == "U13":
+            if age_group in ["U13", "U14"]:
                 return "400"
-            elif age_group == "U15":
+            elif age_group in ["U15", "U16"]:
                 return "500"
-            elif age_group == "U17":
+            elif age_group in ["U17", "U18"]:
                 return "500"
             elif age_group in ("U20", "U23", "SEN", "V35", "V40", "V45"):
                 return "600"
@@ -46,9 +50,11 @@ def get_implement_weight(event_code, gender, age_group):
         if gender == "M":
             if age_group == "U13":
                 return "3.25"
-            elif age_group == "U15":
+            elif age_group == "U14":
+                return "3.00"  # Malta
+            elif age_group in ["U15", "U16"]:
                 return "4.00"
-            elif age_group == "U17":
+            elif age_group in ["U17", "U18"]:
                 return "5.00"
             elif age_group == "U20":
                 return "6.00"
@@ -61,9 +67,11 @@ def get_implement_weight(event_code, gender, age_group):
         elif gender == "F":
             if age_group == "U13":
                 return "2.72"
-            elif age_group == "U15":
+            elif age_group == "U14":
+                return "2.00"
+            elif age_group in ["U15", "U16"]:
                 return "3.00"
-            elif age_group == "U17":
+            elif age_group in ["U17", "U18"]:
                 return "3.00"
             elif age_group in ("U20", "U23", "SEN", "V35", "V40", "V45"):
                 return "4.00"
@@ -72,11 +80,11 @@ def get_implement_weight(event_code, gender, age_group):
 
     elif event_code == "DT":
         if gender == "M":
-            if age_group == "U13":
+            if age_group in ["U13", "U14"]:
                 return "1.00"
-            elif age_group == "U15":
+            elif age_group in ["U15", "U16"]:
                 return "1.25"
-            elif age_group == "U17":
+            elif age_group in ["U17", "U18"]:
                 return "1.50"
             elif age_group == "U20":
                 return "1.75"
@@ -87,7 +95,7 @@ def get_implement_weight(event_code, gender, age_group):
             elif age_group >= "V60":
                 return "1.00"
         elif gender == "F":
-            if age_group == "U13":
+            if age_group in ["U13", "U14"]:
                 return "0.75"
             elif age_group >= "V75":
                 return "0.75"
@@ -96,11 +104,11 @@ def get_implement_weight(event_code, gender, age_group):
 
     elif event_code == "HT":
         if gender == "M":
-            if age_group == "U13":
+            if age_group in ["U13", "U14"]:
                 return "3.00"
-            elif age_group == "U15":
+            elif age_group in ["U15", "U16"]:
                 return "4.00"
-            elif age_group == "U17":
+            elif age_group in ["U17", "U18"]:
                 return "5.00"
             elif age_group == "U20":
                 return "6.00"
@@ -118,9 +126,11 @@ def get_implement_weight(event_code, gender, age_group):
         elif gender == "F":
             if age_group == "U13":
                 return "3.00"
-            elif age_group == "U15":
+            elif age_group == "U14":
+                return "2.00"  # light one in Malta
+            elif age_group in ["U15", "U16"]:
                 return "3.00"
-            elif age_group == "U17":
+            elif age_group in ["U17", "U18"]:
                 return "3.00"
             elif age_group in ("U20", "U23", "SEN", "V35", "V40", "V45"):
                 return "4.00"
--- tests/test_implements.py
+++ tests/test_implements.py
@@ -7,9 +7,14 @@ from athlib import get_implement_weight as weight
 class WeightTests(TestCase):
     """Test suite for implement canned data."""
 
-    def test_a_few(self):
+    def test_a_few_uka_weights(self):
         self.assertEquals(weight("JT", "M", "SEN"), "800")
         self.assertEquals(weight("JT", "F", "V80"), "400")
+
+
+    def test_malta_juniors(self):
+        self.assertEquals(weight("JT", "M", "U14"), "500")
+        self.assertEquals(weight("DT", "F", "U14"), "0.75")
 
 if __name__ == '__main__':
     main()
