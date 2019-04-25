4e31d23aa6d32016c6fcad557b3430b68e19b012
Robin Becker <robin@reportlab.com>
2019-04-25 16:31:23 +0100

    version-->0.3.11

--- athlib/__init__.py
+++ athlib/__init__.py
@@ -3,7 +3,7 @@ from athlib.uka.agegroups import calc_uka_age_group
 from athlib.iaaf_score import score as athlon_score
 from athlib.iaaf_score import performance as athlon_performance_needed
 
-__version__ = u'0.3.10'
+__version__ = u'0.3.11'
 
 from athlib.exceptions import RuleViolation
 
