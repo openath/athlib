03954b4e01b96a21d3f1ef9f1d0616585d969344
Robin Becker <robin@reportlab.com>
2020-02-15 08:52:01 +0000

    bump to 0.5.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ author = u'Andy Robinson, Robin Becker and others'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.5.0'
+version = u'0.5.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
