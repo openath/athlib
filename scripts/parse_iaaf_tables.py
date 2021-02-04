3d2b8bc5c6450337d73c7f1f557b92254c2a72c2
Andrew Robinson <andy@Rostovo.local>
2021-02-04 22:03:15 +0000

    start parsing excel files

--- scripts/parse_iaaf_tables.py
+++ scripts/parse_iaaf_tables.py
@@ -5,5 +5,22 @@ import xlrd
 def run():
     print("This will parse the IAAF tables and output a machine readable file")
 
+
+    for filename in [
+        'data/iaaf_scoring_tables_2017_indoor.xls',
+        'data/iaaf_scoring_tables_2017_outdoors.xls'
+        ]:  
+        print("Reading" + filename)
+
+        book = xlrd.open_workbook(filename)
+        print("   The number of worksheets is {0}".format(book.nsheets))
+
+
+        pointstable = {}
+
+        for sheetnum in range(book.nsheets):
+            pass
+
+
 if __name__=="__main__":
     run()
