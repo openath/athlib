b4d51e948f637c783095f32f9b5be6f2ed07bbb7
Andrew Robinson <andy@Rostovo.local>
2021-02-05 14:00:50 +0000

    more work on parsing

--- scripts/parse_iaaf_tables.py
+++ scripts/parse_iaaf_tables.py
@@ -20,9 +20,12 @@ def run():
     print("This will parse the IAAF tables and output a machine readable file")
 
 
+    points_table = {}
+
+
     for (where, filename) in [
-        ('outdoors', 'data/iaaf_scoring_tables_2017_outdoors.xls'),
-        ('indoor', 'data/iaaf_scoring_tables_2017_indoor.xls')
+        ('OUT', 'data/iaaf_scoring_tables_2017_outdoors.xls'),
+        ('IND', 'data/iaaf_scoring_tables_2017_indoor.xls')
         ]:  
         print("Reading" + filename)
 
@@ -30,12 +33,13 @@ def run():
         print("   The number of worksheets is {0}".format(book.nsheets))
 
 
-        points_table = {}
-        points_table['M'] = OrderedDict()
-        points_table['F'] = OrderedDict()
 
 
         gender = 'M'  # flip it when we see a womens' event, 100-odd pages into the 
+        context = "%s-%s" % (gender, where)
+        points_table[context] = OrderedDict()
+
+
         datapoints = 0
         for sheetnum in range(book.nsheets):
             sheet = book.sheet_by_index(sheetnum)
@@ -51,6 +55,8 @@ def run():
 
             if ('100mH' in headers) or ('50m' in headers):
                 gender = 'F' # half way through book we flip
+                context = "%s-%s" % (gender, where)
+                points_table[context] = OrderedDict()
 
             points_col = headers.index('Points')
             body = matrix[1:]
@@ -61,63 +67,61 @@ def run():
                 for (header, value) in pairs:
                     if header not in ('', 'Points'):
                         event_code = header
-                        if event_code not in points_table[gender]:
-                            points_table[gender][event_code] = []
+                        if event_code not in points_table[context]:
+                            points_table[context][event_code] = []
                         if value != '-':
                             # keep it
-                            points_table[gender][event_code].append((points, value))
+                            points_table[context][event_code].append((points, value))
+                            if (context == 'M-OUT') and (event_code=="100m") and (points > 1390):
+                                print(context, event_code, points, '==>', value)
                             datapoints += 1
+                            if datapoints > 10:
+                                break
 
+        # done looping the input files
 
-        # sanity check, should go up to 1400ish and down near 1
-        maxpoints = minpoints = 700 # midrange
-        for gender in 'MF':
-            p = points_table[gender]
-            for (event_code, stuff) in p.items():
-                for ((points, value)) in stuff:
-                    if points and (points > maxpoints):
-                        maxpoints = points
-                    if points and (points < minpoints):
-                        minpoints = points
+        print("    Extracted %d data points for %s" % (datapoints, context))
 
-        print("    Extracted %d data points - from %d down to %d" % (datapoints, maxpoints, minpoints))
+        print("Event codes by context:")
+        for (context, info) in points_table.items():
+            print("  %s:  %s" % (context, info.keys()))
         # print("    Mens events:" , points_table['M'].keys())
         # print("    Womens events:" , points_table['F'].keys())
 
-        outfilename = os.path.splitext(filename)[0] + '.json'
-        with open(outfilename, 'w') as outfile:
-            json.dump(points_table, outfile)
-        print("    Saved %s" % outfilename)
+    # outfilename = os.path.splitext(filename)[0] + '.json'
+    # with open(outfilename, 'w') as outfile:
+    #     json.dump(points_table, outfile)
+    # print("    Saved %s" % outfilename)
 
-        # now write two CSV files with all event points values
-        # for this we want a sparse matrix, then we can loop over the indices 
+    # # now write two CSV files with all event points values
+    # # for this we want a sparse matrix, then we can loop over the indices 
 
-        spm = {}
-        for (gender, gpointsdict) in points_table.items():
-            for (event_code, event_data) in gpointsdict.items():
-                for (points, value) in event_data:
-                    key = (gender, event_code, points)
-                    spm[key] = value
+    # spm = {}
+    # for (gender, gpointsdict) in points_table.items():
+    #     for (event_code, event_data) in gpointsdict.items():
+    #         for (points, value) in event_data:
+    #             key = (gender, event_code, points)
+    #             spm[key] = value
 
 
-        for gender in 'MF':
-            outfilename = "data/points_table_%s_%s.csv" % (gender, where)
+    # for gender in 'MF':
+    #     outfilename = "data/points_table_%s_%s.csv" % (gender, where)
 
-            with open(outfilename, mode='w') as outfile:
-                gpointsdict = points_table[gender]
-                event_codes = gpointsdict.keys()
-                header = ['Points'] + event_codes
-                print(gender, header)
-                writer = csv.writer(outfile)
-                writer.writerow(header)
-                for points in range(1400, 0, -1):
-                    row = [points]
-                    for event_code in event_codes:
-                        key = (gender, event_code, points)
-                        perf = spm.get(key, None)
-                        row.append(perf)
-                    writer.writerow(row)
-                print("Wrote %s" % outfilename)
+    #     with open(outfilename, mode='w') as outfile:
+    #         gpointsdict = points_table[gender]
+    #         event_codes = gpointsdict.keys()
+    #         header = ['Points'] + event_codes
+    #         print(gender, header)
+    #         writer = csv.writer(outfile)
+    #         writer.writerow(header)
+    #         for points in range(1400, 0, -1):
+    #             row = [points]
+    #             for event_code in event_codes:
+    #                 key = (gender, event_code, points)
+    #                 perf = spm.get(key, None)
+    #                 row.append(perf)
+    #             writer.writerow(row)
+    #         print("Wrote %s" % outfilename)
 
 
 
