6a3de64d55a021af28233af78692e50f89e9e918
Harry Robinson <harry@popocatapetl.local>
2021-02-15 18:49:38 +0000

    score formula beginning to work for track events only

---
+++ tests/test_hungarian_score.py
@@ -0,0 +1,35 @@
+
+
+from unittest import TestCase, main
+from athlib.hungarian_score import get_lookup_table, score
+
+class HunTest(TestCase):
+
+    def test_table_lookup(self):
+        """Tests that 1 + 1 always equals 2."""
+
+        tbl = get_lookup_table()
+        inputs = ('M', 'OUT', '200')
+        values = tbl[inputs]
+        self.assertEquals(values, (5.08, 35.5, 0))
+
+
+    def test_scores(self):
+
+        tests = [
+                ('M', 'OUT', '100', 9.46, 1400),
+                ('M', 'OUT', 'MILE', 240, 918),
+                # ('F', 'OUT', 'LJ', , ) This goes wrong, need to find the field event formula
+            ]
+
+        for test in tests:
+            (gender, inout, event_code, performance, points) = test
+            self.assertEquals(
+                score(gender, inout, event_code, performance), 
+                points
+                )
+
+
+if __name__ == '__main__':
+    main()
+
