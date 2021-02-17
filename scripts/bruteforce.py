f44e97f42a40cb1956800f2de4928689f397102a
Harry Robinson <harry@popocatapetl.local>
2021-02-17 22:38:40 +0000

    first attempts at brute force scoring system

--- scripts/bruteforce.py
+++ scripts/bruteforce.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import json
+sys.path.insert(0, '..')
+import athlib
+
+from athlib.codes import FIELD_EVENTS
 
 FILES = [
     'data/iaaf_scoring_tables_2017_outdoors.json',
@@ -43,36 +47,52 @@ def score(gender, inout, event_code, performance):
     sc = load_scores()
     key = (gender, inout, event_code)
     series = sc[key]
-    print("loaded series with %d data points" % len(series))
 
+
+    cleaned_series = []
+    for (perf, points) in series:
+        if perf and points:
+            cleaned_series.append((perf, points))
+    series = cleaned_series
     # binary search for the performance we need.  Somewhere between 0 and 1400 points
 
-    upper = 1400
-    lower = 0
-    mid = 700
-    itera = 0
-    while True:
-        itera += 1
-        print("Iteration %d:  upper=%d, lower=%d, mid=%d" % (itera, upper, lower, mid))
-        mid = int((upper + lower) / 2)
-        perf = series[mid]
-        one_below = series[mid - 1]
+    # for world records
+    (maxpoints, bestperf) = series[0]
+    if event_code in FIELD_EVENTS:
+        if performance > bestperf:
+            return maxpoints
+    else:
+        if performance < bestperf:
+            return maxpoints
 
-        if performance > perf:
-            lower = mid
-        elif performance < one_below:
-            upper = mid
-        else: #bullseye
-            return mid
+    for x in range(len(series)-1): 
+        (points1, perf1) = series[x]
+        (points2, perf2) = series[x+1]
 
-        if mid == 1400:
-            return 1400
-        elif mid == 0:
-            return 0
+        if event_code in FIELD_EVENTS:
+            # do something
+            
+            if (performance >= perf2) and (performance < perf1):
+                return points2
+
+        else:
+
+            if (performance < perf2) and (performance >= perf1):
+                return points1
+    return 0
+
+
 
     return 0
 
 if __name__=='__main__':
     sc = load_scores()
 
-    print("10sec in Mens 100 =", score('M', 'OUT', '100', 10.0))
+    print("9sec in Mens 100 =", score('M', 'OUT', '100', 9.0))    
+    print("110m in Mens Javelin =", score('M', 'OUT', 'JT', 110.0))    
+
+    print("12sec in Mens 100 =", score('M', 'OUT', '100', 12.0))    
+    print("50m in Mens Javelin =", score('M', 'OUT', 'JT', 50.0))    
+
+    print("30sec in Mens 100 =", score('M', 'OUT', '100', 30.0))    
+    print("1m in Mens Javelin =", score('M', 'OUT', 'JT', 1.0))    
