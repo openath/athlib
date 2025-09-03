36446efd51834ceb9859db5d902eb566a337b55c
Andy Robinson <andy@opentrack.run>
2025-09-03 12:46:33 +0100

    Removed RR as it's a synonym for FR (frame running).  Bumped version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ author = u'Andy Robinson, Robin Becker, Llewellyn Brunsdon-Haland, James Davis, 
 # built documents.
 #
 # The short X.Y version.
-version = u'0.9.1'
+version = u'0.9.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
